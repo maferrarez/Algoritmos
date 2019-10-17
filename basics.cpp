80894e2f72756242ef89951bde6edf40d5ba1783
maferrarez <50337599+maferrarez@users.noreply.github.com>
2019-10-17 09:16:46 -0300

    Add files via upload

---
+++ basics.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "PORTUGUESE");
+	
+
+
+
+system("pause");
+return 0;
+}
